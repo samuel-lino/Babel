b5506e86f78c119b8e51428e5bde7add8d72fc40
samuel <samuellino2006@gmail.com>
2025-04-07 11:49:07 -0300

    exercicio concluido

---
+++ src/exercicio.js
@@ -0,0 +1,11 @@
+const notasDosAlunos = [{nome: 'Lucas', nota: 7},{nome: 'Maria', nota: 5},{nome: 'Antonio', nota: 3},{nome: 'Gabriela', nota: 9},]
+
+function mostrarAlunoAprovado(listaDeAlunos){
+    return listaDeAlunos.filter((e)=>{
+        return e.nota > 6
+    })
+}
+
+const aprovados = mostrarAlunoAprovado(notasDosAlunos)
+
+console.log(aprovados)
